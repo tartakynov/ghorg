0bf1422a7205676d4ac31ba86cf8f5cdec452ecc
Artem Tartakynov <a.tartakynov@2gis.ru>
2015-08-15 19:43:52 +0600

    refactored

--- collect.py
+++ collect.py
@@ -11,57 +11,64 @@ from urlparse import urlparse
 from sqlalchemy import func
 from py4j.java_gateway import JavaGateway
 
-def getConfig():
-    with open('config.json', 'rb') as fin:
-        return json.load(fin)
-
-def normalizeUrl(url):
-    if not url:
-        return None
-    if '://' not in url:
-        url = 'http://' + url
-    scheme, netloc, path, params, query, fragment, = urlparse(url)
-    return '%s://%s/%s%s' % (scheme, netloc.rstrip('/'), path.lstrip('/'), '?' + query if query else '')
-
-def getQueryResults():
-    print 'fuck'
-    with open('gharchive/query-results.csv', 'rb') as fin:
-        reader = csv.reader(fin)
-        next(reader)
-        return [(name, int(forks)) for name, forks in reader]
-
-def main():
-    config = getConfig();
-    db = data.connect(config['db'])
-
+if __name__ == '__main__':
     gateway = JavaGateway()
     locationApp = gateway.entry_point
+    with open('config.json', 'rb') as fin:
+        config = json.load(fin)
+
+    def normalizeUrl(url):
+        if not url:
+            return None
+        if '://' not in url:
+            url = 'http://' + url
+        scheme, netloc, path, params, query, fragment, = urlparse(url)
+        return '%s://%s/%s%s' % (scheme, netloc.rstrip('/'), path.lstrip('/'), '?' + query if query else '')
+
+    def queryResults():
+        with open('gharchive/query-results.csv', 'rb') as fin:
+            reader = csv.reader(fin)
+            next(reader)
+            return [(name, int(forks)) for name, forks in reader]
+
+    def obeyRateLimit(headers):
+        remaining = int(headers['X-RateLimit-Remaining'])
+        reset = long(headers['X-RateLimit-Reset'])
+        if remaining < 3:
+            tts = reset - time.time()
+            print 'sleeping for %f sec due to GitHub rate limit' % tts
+            time.sleep(tts)
+
+    def createOrganization(name, forks):
+        request = urllib2.urlopen('https://api.github.com/orgs/%s?access_token=%s' % (name, config['access_token']))
+        obeyRateLimit(request.headers)
+        response = json.load(request)
+        return Organization(
+            id = response['id'],
+            name = response['login'],
+            location = response.get('location', None),
+            public_repos = response.get('public_repos', 0),
+            forks_2014 = forks,
+            url_github = response.get('html_url', None),
+            url_site = normalizeUrl(response.get('blog', None)),
+            created_at = parse(response['created_at']),
+            suggested_country = locationApp.countryCode(response.get('location', None))
+        )
+
+    db = data.connect(config['db'])
     with db.session(autocommit = True) as session:
-        disappearedOrgs = set([v for v, in session.query(NotFound.name).all()])
-        lines = getQueryResults()
-        for pos, (name, forks) in enumerate(lines, start = 1):
+        disappeared = set([v for v, in session.query(NotFound.name).all()])
+        rows = queryResults()
+        for pos, (name, forks) in enumerate(rows, start = 1):
             startTime = time.time()
             org = session.query(Organization).filter(func.lower(Organization.name) == name).first()
             if org:
                 org.forks_2014 = forks
                 org.suggested_country = locationApp.countryCode(org.location)
-            elif name not in disappearedOrgs:
+            elif name not in disappeared:
                 try:
-                    request = urllib2.urlopen('https://api.github.com/orgs/%s?access_token=%s' % (name, config['access_token']))
-                    response = json.load(request)
-                    session.add(
-                        Organization(
-                            id = response['id'],
-                            name = response['login'],
-                            location = response.get('location', None),
-                            public_repos = response.get('public_repos', 0),
-                            forks_2014 = forks,
-                            url_github = response.get('html_url', None),
-                            url_site = normalizeUrl(response.get('blog', None)),
-                            created_at = parse(response['created_at']),
-                            suggested_country = locationApp.countryCode(response.get('location', None))
-                        )
-                    )
+                    org = createOrganization(name, forks)
+                    session.add(org)
                 except urllib2.HTTPError as e:
                     if e.code <> 404:
                         raise
@@ -69,8 +76,4 @@ def main():
                     session.add(NotFound(name = name))
 
             elapsed = (time.time() - startTime) * 1000
-            print '%d. %s, elapsed %dms, left %d' % (pos, name, elapsed, len(lines) - pos)
-
-
-if __name__ == '__main__':
-    main()
+            print '%d. %s, elapsed %dms, left %d' % (pos, name, elapsed, len(rows) - pos)
